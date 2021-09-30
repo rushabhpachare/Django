36100adad259db7cc1c3e011c1ccafdbe27deb65
RUSHABH PACHARE <70842378+rushabhpachare@users.noreply.github.com>
2021-09-30 11:52:15 +0530

    Add files via upload

--- firstapp/firstapp/urls.py
+++ firstapp/firstapp/urls.py
@@ -20,10 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
     path('removepunc', views.removepunc, name='removepunc'),
-    path('capitalizefirst', views.capitalizefirst, name='capitalizefirst'),
-    path('newlineremove', views.newlineremove, name='newlineremove'),
-    path('spaceremove', views.spaceremove, name='spaceremove'),
-    path('charcount', views.charcount, name='charcount'),
 ]
--- firstapp/firstapp/views.py
+++ firstapp/firstapp/views.py
@@ -1,25 +1,15 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('''<h1>One Batch Two Batch <br/> Link for the websites <br/><a href="https://www.netflix.com/in/"> NETFLIX </a>  <br/><a href="https://www.hotstar.com/in"> HOTSTAR </a>  <br/><a href="https://www.google.com/">GOOGLE</a>  </h1> <br/> <h3> Go to next page and back button </h3> <br/> <h3> <a href="/removepunc"> Remove Punc </a><br/>  <a href="/capitalizefirst"> Capitalize First </a><br/>  <a href="/newlineremove"> New Line Remove </a><br/>  <a href="/spaceremove"> Space Remove </a><br/>  <a href="/charcount"> Char Count </a><br/>  </h3>''')
-
-
-def about(request):
-    return HttpResponse('''<h3>Penny and Dime  <br/><a href="https://www.marvel.com/tv-shows/marvel-s-daredevil/3"> DAREDEVIL </a>  <br/><a href="https://www.netflix.com/in/title/80002566"> DEFENDERS </a>  <br/><a href="https://www.netflix.com/in/title/80002612"> IRON FIST</a>  </h3>''')
+    params = {'name' : 'Rushabh', 'place': 'India'}
+    return render(request, 'index.html', params)
 
 
 def removepunc(request):
+    #Get the text
+    djtext = request.GET.get('text', 'default')
+    print(djtext)
+    #Analyze the text
     return HttpResponse('''removepunc <br/> <a href="/"> Back </a>''')
 
-def capitalizefirst(request):
-    return HttpResponse('''capitalizefirst <br/> <a href="/"> Back </a>''')
-
-def newlineremove(request):
-    return HttpResponse('''newlineremove <br/> <a href="/"> Back </a>''')
-
-def spaceremove(request):
-    return HttpResponse('''spaceremove <br/> <a href="/"> Back </a>''')
-
-def charcount(request):
-    return HttpResponse('''charcount <br/> <a href="/"> Back </a>''')
